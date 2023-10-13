32f717f2f3f83fa924102e20e141d9dfbea2ae48
ijongmin <scott.lee@handys.co.kr>
2023-10-13 23:02:53 +0900

    chore: 스프링부트 버전 다운그레이드

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
-	id("org.springframework.boot") version "3.1.4"
+	id("org.springframework.boot") version "2.7.9"
 	id("io.spring.dependency-management") version "1.1.3"
 	kotlin("jvm") version "1.8.22"
 	kotlin("plugin.spring") version "1.8.22"
